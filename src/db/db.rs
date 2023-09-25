28cb9786105d58c7ea3ca2bfbefb0acbecc30250
Casey Clayton <claytoncasey01@gmail.com>
2023-09-25 04:44:19 -0400

    Add initial structure of the file

---
+++ src/db/db.rs
@@ -0,0 +1,50 @@
+use crate::db::metadata::MetaData;
+use anyhow::Result;
+use byteorder::{BigEndian, WriteBytesExt};
+use fs2::FileExt;
+use std::{
+    fs::File,
+    io::{Seek, SeekFrom, Write},
+};
+
+// AlloyDB Database File
+const MAGIC_NUMBER: &[u8; 4] = b"ADBF";
+const VERSION: u16 = 1;
+
+pub struct AlloyDB {
+    file_path: String,
+}
+
+impl AlloyDB {
+    pub fn new(file_path: String) -> Self {
+        Self { file_path }
+    }
+
+    pub fn create(&self, metadata: MetaData) -> Result<()> {
+        let mut file = File::create(&self.file_path)?;
+
+        // Lock the file for exclusive access
+        file.lock_exclusive()?;
+
+        // Write header information
+        file.write_all(MAGIC_NUMBER)?;
+        file.write_all(&VERSION.to_be_bytes())?;
+
+        // Initial offsets
+        let initial_metadata_offset: u64 = file.seek(SeekFrom::Current(0))? + 24;
+        let initial_index_offset: u64 = initial_metadata_offset + metadata.size() as u64;
+        let initial_vector_data_offset: u64 = initial_index_offset;
+
+        // Write our offsets to the file.
+        file.write_u64::<BigEndian>(initial_metadata_offset)?;
+        file.write_u64::<BigEndian>(initial_index_offset)?;
+        file.write_u64::<BigEndian>(initial_vector_data_offset)?;
+        // Total records, starting at 0
+        file.write_u64::<BigEndian>(0)?;
+
+        // Unlock the file
+        file.unlock()?;
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,1 @@
 pub mod db;
-pub mod tokenizer;
-pub mod vectorizor;
-
-pub use db::Db;
